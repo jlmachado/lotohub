ee1645ff54333562b0df11f5f5a52567cd0ba899
João De Lima <jao-lm@hotmail.com>
2026-03-09 13:57:33 +0000

    ## Error Type Runtime Error
    ## Error Message
    Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'media.api-sports.io',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
