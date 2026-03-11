179014632ff8233a5b8cd0ea77fa7580b46c3e43
João De Lima <jao-lm@hotmail.com>
2026-03-11 18:16:43 +0000

    usar esse banco de dados studio-4690612286-70e98

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -4,9 +4,9 @@
 
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || 'studio-4690612286-70e98.firebaseapp.com',
+  projectId: 'studio-4690612286-70e98',
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || 'studio-4690612286-70e98.appspot.com',
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
