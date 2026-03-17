1cdfcc2f950d261eb5f8c6373c5ad8fc268499a1
João De Lima <jao-lm@hotmail.com>
2026-03-17 23:19:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6619904012-2fe26",
+  "appId": "1:461885009111:web:8c1f2a7d5e692060af9492",
+  "apiKey": "AIzaSyA_Ar19kaPbYlGuPu4a7Pui2hY-nCMO5TE",
+  "authDomain": "studio-6619904012-2fe26.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "461885009111"
+};
